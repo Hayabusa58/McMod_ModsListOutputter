5f552dff2e5a04b73aea0bf4e5c839d67f7ebf41
Hayabusa <yuuta.keiko.k@live.jp>
2016-04-10 13:19:41 +0900

    Add feature : Generated in any of the directory specified in the configuration. -add コンフィグで指定した任意のディレクトリにリストを生成できるように -change コンフィグファイル名をModNameで生成できるように実装 -modify その他細かい変更・実装 謝辞；この機能に関して，defeatedcrow様の助言を頂きました。ありがとうございました。

--- src/main/java/me/peregrine/MLOConfig.java
+++ src/main/java/me/peregrine/MLOConfig.java
@@ -2,31 +2,52 @@ package me.peregrine;
 
 import java.io.File;
 
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.common.config.Configuration;
 
 public class MLOConfig {
 
 	public static boolean isActive = true;
     public static String format = "Modid:{modid} Modname:{name} Modversion:{version}[{displayVersion}] Filename:{source}";
+    public static String listdir;
+    //public static String listname;
     public static boolean isActivatedOnly = false;
 
 	public static void preLoad(File file)
 	{
+		//File isFileflag = new File(Minecraft.getMinecraft().mcDataDir, "config/ModsListOutPutter.cfg");
+		
+		
+ 		
 		Configuration cfg = new Configuration(file, "1.0");
+		ModsListOutputter.DebugMessage("コンフィグ生成");
+		
+		
 
 		try {
 
 			cfg.load();
 
-			System.out.println("コンフィグ読み込み");
+			listdir = new File(Minecraft.getMinecraft().mcDataDir, "mods/ModsList.txt").getParent();
+
+			//String mcdir = mc.getPath();
+
+			ModsListOutputter.DebugMessage("コンフィグ読み込み");
+			//System.out.println("コンフィグ読み込み");
+			System.out.println(listdir);
+			//System.out.println(listdir);
 			isActive = cfg.getBoolean("1.isActive", "General", isActive, "Whether this mod is valid.");
 			format = cfg.getString("1.format", "Style", format, "Output format");
+			listdir = cfg.getString("3.GenerateDirectory", "General", listdir , "Directory of generate modslist file. Default directory is [../Gamedirectory/mods].");
 			isActivatedOnly = cfg.getBoolean("2.isActivatedOnly", "General", isActivatedOnly,
 					"Write only Active Mods.");
+			System.out.println(listdir);
 
 		} finally {
 			cfg.save();
-			System.out.println("コンフィグセーブ");
+			ModsListOutputter.DebugMessage("コンフィグセーブ");
 		}
-	}
+		}
+	
+	
 }
--- src/main/java/me/peregrine/ModsListOutputter.java
+++ src/main/java/me/peregrine/ModsListOutputter.java
@@ -3,7 +3,6 @@ package me.peregrine;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.util.List;
@@ -11,44 +10,36 @@ import java.util.List;
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.ModContainer;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import net.minecraft.client.Minecraft;
 
 @Mod(name = "ModsList Outputter", modid = "me.peregrine.modslistoutputter", version = "MC1.7.x_1.0a")
 public class ModsListOutputter
 {
+	@Instance("me.peregrine.modslistoutputter")
+	public static ModsListOutputter MLO;
+
 	public static boolean isIDFound = false;
 
 	@EventHandler
     public void preLoad(FMLPreInitializationEvent event)
     {
-    	MLOConfig.preLoad(event.getSuggestedConfigurationFile());
-    	  //throw new MLOMissingIDException("");
-    	try {
-			this.ChangeConfigName();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	    File file = new File(event.getModConfigurationDirectory(), "ModsListOutPutter.cfg");
+	    MLOConfig.preLoad(file);
+    	//MLOConfig.preLoad(event.getSuggestedConfigurationFile());
 
     }
-
-
-
-
-
-
 
     @EventHandler
     public void postLoad(FMLPostInitializationEvent event)
     {
 
-
-
     if(MLOConfig.isActive)
     {
-    	System.out.println("アクティブだった");
+    	//System.out.println("アクティブだった");
+    	//ModsListOutputter.DebugMessage("コンフィグ読み込み");
     	StringBuilder sb = new StringBuilder();
         List<ModContainer> mods = Loader.instance().getModList();
 
@@ -59,6 +50,7 @@ public class ModsListOutputter
 
         for(ModContainer mod : mods)
         {
+        	ModsListOutputter.DebugMessage("mod情報取得");
             String buf = MLOConfig.format;
             buf = buf.replace("{modid}", mod.getModId());
             buf = buf.replace("{name}", mod.getName());
@@ -69,17 +61,20 @@ public class ModsListOutputter
             sb.append("\n");
         }
 
-
         try
         {
-        	System.out.println("アクティブだった2");
+        	//System.out.println("アクティブだった2");
         	//StringBuilder sb2 = new StringBuilder();
-            File file = new File(Minecraft.getMinecraft().mcDataDir, "mods/ModsList.txt");
-            FileOutputStream fos = new FileOutputStream(file);
+            File modslist = new File(MLOConfig.listdir, "Modslist.txt");
+            modslist.createNewFile();
+        	//File modslist = new File(Minecraft.getMinecraft().mcDataDir, "mods/ModsList.txt");
+            ModsListOutputter.DebugMessage("リスト生成");
+            FileOutputStream fos = new FileOutputStream(modslist);
             PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(fos, "UTF-8")));
             pw.write(sb.toString());
             pw.close();
             fos.close();
+
         }
         catch (Exception e){
 
@@ -88,12 +83,16 @@ public class ModsListOutputter
         }
     }
     }
-    public static void ChangeConfigName() throws IOException
+    /*public void ChangeConfigName() throws IOException
     {
     	File oldcfg = new File(Minecraft.getMinecraft().mcDataDir, "/config/me.peregrine.modslistoutputter.cfg");
     	File newcfg = new File(Minecraft.getMinecraft().mcDataDir, "/config/ModsListOutPutter.cfg");
     if(oldcfg.exists())
     	oldcfg.renameTo(newcfg);
     }
-
+*/
+    public static void DebugMessage(String pText, Object... pData)
+    {
+    	System.out.println(String.format("ModsListOutPutter +" + pText, pData));
+    }
 }
