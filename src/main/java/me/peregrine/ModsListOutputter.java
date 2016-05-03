619678d6093232bda14e0e20a38d77b545f61947
Hayabusa <yuuta.keiko.k@live.jp>
2016-05-03 21:14:20 +0900

    Add BBCodeMode. -add BBCodeに最適化したリストを生成するモードを追加。コンフィグで切り替え可能。 -modify デバッグメッセージを除去，コードのリファクタリング

--- src/main/java/me/peregrine/ModsListOutputter.java
+++ src/main/java/me/peregrine/ModsListOutputter.java
@@ -15,42 +15,42 @@ import cpw.mods.fml.common.ModContainer;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
+/*
+ *
+ * The main class of ModslistOutPutter
+ * @auther Hayabusa
+ *
+ */
 @Mod(name = "ModsList Outputter", modid = "me.peregrine.modslistoutputter", version = "MC1.7.x_1.0a")
 public class ModsListOutputter
 {
 	@Instance("me.peregrine.modslistoutputter")
 	public static ModsListOutputter MLO;
 
-	public static boolean isIDFound = false;
+	//public static boolean isIDFound = false;
+
+	public static boolean istheresecondarydistribution = false;
 
 	@EventHandler
-    public void preLoad(FMLPreInitializationEvent event)
-    {
+    public void preLoad(FMLPreInitializationEvent event){
 	    File file = new File(event.getModConfigurationDirectory(), "ModsListOutPutter.cfg");
 	    MLOConfig.preLoad(file);
-    	//MLOConfig.preLoad(event.getSuggestedConfigurationFile());
-
     }
 
     @EventHandler
-    public void postLoad(FMLPostInitializationEvent event)
-    {
+    public void postLoad(FMLPostInitializationEvent event){
 
-    if(MLOConfig.isActive)
-    {
-    	//System.out.println("アクティブだった");
-    	//ModsListOutputter.DebugMessage("コンフィグ読み込み");
+    if(MLOConfig.isactive){
     	StringBuilder sb = new StringBuilder();
         List<ModContainer> mods = Loader.instance().getModList();
 
-        if(MLOConfig.isActivatedOnly)
-        {
+        if(MLOConfig.isactivatedonly){
             mods = Loader.instance().getActiveModList();
         }
 
-        for(ModContainer mod : mods)
-        {
+        for(ModContainer mod : mods){
         	ModsListOutputter.DebugMessage("mod情報取得");
+        	//コンフィグからフォーマットを取得して置換
             String buf = MLOConfig.format;
             buf = buf.replace("{modid}", mod.getModId());
             buf = buf.replace("{name}", mod.getName());
@@ -59,40 +59,103 @@ public class ModsListOutputter
             buf = buf.replace("{source}", mod.getSource().getName());
             sb.append(buf);
             sb.append("\n");
+
+        }try{
+        	this.GenerateModslist(sb);
+        	/*if(MLOConfig.isBBCodemodeactive){
+        		this.UpdateforBBCode();
+        	}*/
+
+
+        } catch (Exception e){
+        	e.printStackTrace();
         }
 
-        try
+     }
+
+    }
+    /*デバッグメッセージ*/
+    public static void DebugMessage(String pText, Object... pData){
+    	System.out.println(String.format("ModsListOutPutter + " + pText, pData));
+    }
+
+
+    public File GenerateModslist(StringBuilder sb)
+    {
+    	try
         {
-        	//System.out.println("アクティブだった2");
-        	//StringBuilder sb2 = new StringBuilder();
             File modslist = new File(MLOConfig.listdir, "Modslist.txt");
             modslist.createNewFile();
-        	//File modslist = new File(Minecraft.getMinecraft().mcDataDir, "mods/ModsList.txt");
-            ModsListOutputter.DebugMessage("リスト生成");
+            System.out.println("リスト生成");
             FileOutputStream fos = new FileOutputStream(modslist);
             PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(fos, "UTF-8")));
-            pw.write(sb.toString());
-            pw.close();
-            fos.close();
-
+            if(MLOConfig.isBBCodemodeactive){
+            	pw.write("[spoiler]");
+            	pw.write(sb.toString());
+            	pw.write("[/spoiler]");
+            	pw.close();
+            	fos.close();
+            }else{
+            	pw.write(sb.toString());
+            	pw.close();
+            	fos.close();
+            }
+            return modslist;
+        } catch (Exception e){
+        	return null;
         }
-        catch (Exception e){
 
-
-
-        }
     }
-    }
-    /*public void ChangeConfigName() throws IOException
+    /* うまくいかなかったので未使用. いつか実装したい*/
+    public boolean isThereSecondaryDistribution(String str)
     {
-    	File oldcfg = new File(Minecraft.getMinecraft().mcDataDir, "/config/me.peregrine.modslistoutputter.cfg");
-    	File newcfg = new File(Minecraft.getMinecraft().mcDataDir, "/config/ModsListOutPutter.cfg");
-    if(oldcfg.exists())
-    	oldcfg.renameTo(newcfg);
+    	if(str.matches(".*1.7.10-Mod.*|.*1.8-Mod*.|*.1.8.9-Mod.*|*.1.9-Mod.*")){
+    		istheresecondarydistribution = true;
+    		return true;
+    	}else{
+    		return false;
+    	}
+
     }
-*/
-    public static void DebugMessage(String pText, Object... pData)
-    {
-    	System.out.println(String.format("ModsListOutPutter +" + pText, pData));
+
+   /* public void UpdateforBBCode(){
+    	if(MLOConfig.isBBCodemodeactive){
+    		File file = new File(MLOConfig.listdir, "Modslist.txt");
+    		File dummyfile = new File(MLOConfig.listdir, "Modslist2.txt");
+        	if(file.exists()){
+        		try{
+        			System.out.println("OK!!!");
+
+        			FileReader fr = new FileReader(file);
+        			BufferedReader br = new BufferedReader(fr);
+
+        			FileWriter fw = new FileWriter(dummyfile);
+        			PrintWriter pr = new PrintWriter(fw);
+
+        			String line;
+        			int i = 0;
+
+        			while((line = br.readLine())!= null){
+        				pr.println(line);
+        				i++;
+        				if(i == 1){
+        					pr.println("[spoiler]");
+        				}
+        				pr.println("[/spoiler]");
+
+        				br.close();
+        				pr.close();
+        			}
+        		} catch (Exception e){
+
+        		}
+        	}
+        }*/
+
+
     }
-}
+
+
+
+
+
